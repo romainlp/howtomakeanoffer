8144d8464ce81d0152e56d13df4276c4591a6a35
Romain Le Polh <romainlepolh@gmail.com>
2019-04-11 22:44:04 +0930

    Remove contributors

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -3,7 +3,6 @@
     <ul>
       <li><router-link to="/">Get an offer</router-link></li>
       <li><router-link to="/about">About</router-link></li>
-      <li><router-link to="/contributors">Contributors</router-link></li>
     </ul>
   </nav>
 </template>
--- src/views/Contributors.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div class="contributors page">
-    <h1>Contributors</h1>
-    <div class="content">
-      <p>@todo Get list of contributors with Github API</p>
-    </div>
-  </div>
-</template>
