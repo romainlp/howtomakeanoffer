8807686caf0e7a2595c8390146686e6610b24bf0
Romain Le Polh <romainlepolh@gmail.com>
2019-04-12 11:56:49 +0930

    Add how to buy guide

--- src/views/Guide.vue
+++ src/views/Guide.vue
@@ -2,7 +2,37 @@
   <div class="guide page">
     <h1>How to buy</h1>
     <div class="content">
-      <p>@todo</p>
+      <h2>First contact</h2>
+      <p>Use less work as possible, limit it to:</p>
+      <ul>
+        <li>"Is it available?" or even better "available?"</li>
+        <li>"150$?"</li>
+        <li>"Location?"</li>
+        <li><b>Never user any greetings</b></li>
+      </ul>
+      <h2>Quantity over quality</h2>
+      <p>You want to contact as much seller as possible, even if you are not really interested.<br>
+      Always ask them few questions. And never give them any updates.</p>
+      <h2>Don't show up</h2>
+      <p>Ask when you can inspect the goods, set a time, and never go. It gives you a strong psychological advantage.</p>
+      <h2>Be low</h2>
+      <p>Keep in mind, if someone is advertising something for 300 dollars, there is few chances she/he might
+        be keen to sell it for less than half of the given price, <b>TRY</b>.</p>
     </div>
   </div>
 </template>
+
+<style lang="scss">
+.content {
+  background: #fff;
+  border-radius: 4px;
+  padding: 30px;
+  ul {
+    display: inline-block;
+    margin: 1em 0;
+    li {
+      text-align: left;
+    }
+  }
+}
+</style>
