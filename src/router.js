89571aa25d958f793d30359bd0552e256f113728
Romain Le Polh <romainlepolh@gmail.com>
2019-04-12 11:44:24 +0930

    Update webpackChunkName

--- src/router.js
+++ src/router.js
@@ -53,18 +53,12 @@ const router = new Router({
     {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
     },
     {
       path: '/guide',
       name: 'guide',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/Guide.vue')
+      component: () => import(/* webpackChunkName: "guide" */ './views/Guide.vue')
     }
   ]
 })
